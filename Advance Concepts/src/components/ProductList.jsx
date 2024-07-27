eeb545826f76c6dba24a48aeef456c7be435e303
Zynalex9 <alzn952@gmail.com>
2024-07-27 15:39:28 +0500

    Error Handling

--- Advance Concepts/src/components/ProductList.jsx
+++ Advance Concepts/src/components/ProductList.jsx
@@ -1,34 +1,71 @@
-import React, { useCallback, useEffect, useState } from 'react'
-import { useFetch } from '../hooks/useFetch'
+import React, { useCallback, useEffect, useState } from "react";
+import { useFetch } from "../hooks/useFetch";
 function ProductList() {
-    const [url, setURL ] = useState('http://localhost:8000/products')
-  const {data:products,loading} =  useFetch(url)
+  const [url, setURL] = useState("http://localhost:8000/products");
+  const { data: products, loading,error} = useFetch(url);
+
+  return (
+    <section>
+      <div className="">
+        {loading && <p>Loading Products</p>}
+        {error && <p>{error}</p>}
+        
+        </div>
     
-    return (
-        <section>
-            <div className="">{loading &&<p>Loading Products</p>}</div>
-            <div className="filter">
+      <div className="filter">
+        <button onClick={() => setURL("http://localhost:8000/products")}>
+          All
+        </button>
+        <button
+          onClick={() =>
+            setURL("http://localhost:8000/products?category=React+Course")
+          }
+        >
+          React Courses
+        </button>
+        <button
+          onClick={() =>
+            setURL("http://localhost:8000/products?category=JS+Course")
+          }
+        >
+          JS Courses
+        </button>
+        <button
+          onClick={() =>
+            setURL("http://localhost:8000/products?category=HTML+Course")
+          }
+        >
+          HTML Courses
+        </button>
+        <button
+          onClick={() =>
+            setURL("http://localhost:8000/products?category=Python+Course")
+          }
+        >
+          Pyton Courses
+        </button>
+        <button
+          onClick={() => setURL("http://localhost:8000/products?in_stock=true")}
+        >
+          In Stock Only
+        </button>
+      </div>
 
-            <button onClick={()=> setURL('http://localhost:8000/products')}>All</button>
-            <button onClick={()=> setURL('http://localhost:8000/products?category=React+Course')}>React Courses</button>
-            <button onClick={()=> setURL('http://localhost:8000/products?category=JS+Course')}>JS Courses</button>
-            <button onClick={()=> setURL('http://localhost:8000/products?category=HTML+Course')}>HTML Courses</button>
-            <button onClick={()=> setURL('http://localhost:8000/products?category=Python+Course')}>Pyton Courses</button>
-            <button onClick={()=> setURL('http://localhost:8000/products?in_stock=true')}>In Stock Only</button>
-            </div>
+      {products &&
+        products.map((product) => (
+          <div className="card" key={product.id}>
+            <p className="id">{product.id}</p>
+            <p className="name">{product.name}</p>
+            <p className="info">
+              <span>${product.price}</span>
+              <span className={product.in_stock ? "instock" : "unavailable"}>
+                {product.in_stock ? "In Stock" : "Unavailable"}
+              </span>
+            </p>
+          </div>
+        ))}
+    </section>
+  );
+}
 
-            { products && products.map((product) => (
-                <div className="card" key={product.id}>
-                    <p className="id">{product.id}</p>
-                    <p className="name">{product.name}</p>
-                    <p className="info">
-                        <span>${product.price}</span>
-                        <span className={product.in_stock ? "instock" : "unavailable"}>{product.in_stock ? "In Stock" : "Unavailable"}</span>
-                    </p>
-                </div>            
-            )) }
-        </section>
-      )
-    }
-
-export default ProductList
+export default ProductList;
--- Advance Concepts/src/hooks/useFetch.js
+++ Advance Concepts/src/hooks/useFetch.js
@@ -1,17 +1,27 @@
-import { useEffect, useState } from "react"
+import { useEffect, useState } from "react";
 
-export const useFetch=(url)=>{
-const [data,setData] = useState(null)
-const [ loading, setLoading] = useState(false)
-useEffect(()=>{
-const fetchData = async ()=>{
-    setLoading(true)
-    const res = await fetch(url)
-    const result = await res.json()
-    setLoading(false)
-    setData(result)
-}
-fetchData()
-},[url])
-return {data,loading}
-}
+export const useFetch = (url) => {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("")
+  useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true);
+      try {
+        const res = await fetch(url);
+        if (!res.ok) {
+          throw new Error(res.statusText);
+        }
+        const result = await res.json();
+        setLoading(false);
+        setData(result);
+        setError(false)
+      } catch (error) {
+        setLoading(false);
+        setError("Failed to Fetch Data, Server is down")
+      }
+    };
+    fetchData();
+  }, [url]);
+  return { data, loading, error};
+};
