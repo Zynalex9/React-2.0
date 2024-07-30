d8c7ec682ed10339a983a0debdc8f5708621da22
Zynalex9 <alzn952@gmail.com>
2024-07-30 12:16:22 +0500

    Basic Router Setup

--- React Router/src/App.jsx
+++ React Router/src/App.jsx
@@ -1,12 +1,28 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import { Routes, Route } from "react-router-dom";
+import Home from "./components/Home";
+import ProductList from "./components/ProductList";
+import ProductDetail from "./components/ProductDetail";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 import "./App.css";
+import Contact from "./components/Contact";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  return <></>;
+  return (
+    <div className="App">
+      <Header />
+      <main>
+        <Routes>
+          <Route path="/" element={<Home />}/>
+          <Route path="products" element={<ProductList />}/>
+          <Route path="contact" element={<Contact />}/>
+          <Route path="products/1001" element={<ProductDetail />}/>
+        </Routes>
+      </main>
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
---
+++ React Router/src/components/ProductDetail.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function ProductDetail() {
+  return (
+    <div>
+      <h1>Hello Product Details</h1>
+    </div>
+  )
+}
+
+export default ProductDetail
