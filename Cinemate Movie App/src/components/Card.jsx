4cfa8e0909330f84fe36f2feb884ccfeb0e2b149
Zynalex9 <alzn952@gmail.com>
2024-08-01 10:00:53 +0500

    Card Design

--- Cinemate Movie App/src/components/Card.jsx
+++ Cinemate Movie App/src/components/Card.jsx
@@ -1,7 +1,26 @@
 import React from "react";
-
-function Card() {
-  return <div></div>;
+import { Link } from "react-router-dom";
+function Card({movies}) {
+ const {id,title,overview,poster_path} = movies
+ console.log(movies)
+  return (
+    <div className="max-w-sm bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700 m-3">
+      <Link to="#">
+        <img className="rounded-t-lg" src={`https://image.tmdb.org/t/p/w500/${poster_path}`} alt="" />
+      </Link>
+      <div className="p-5">
+        <Link to="#">
+          <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+            {title}
+          </h5>
+        </Link>
+        <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+          {overview}
+        </p>
+        
+      </div>
+    </div>
+  );
 }
-
+// https://image.tmdb.org/t/p/w500/
 export default Card;
--- Cinemate Movie App/src/pages/MovieList.jsx
+++ Cinemate Movie App/src/pages/MovieList.jsx
@@ -1,11 +1,27 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { Card } from "../components";
 
 function MovieList() {
+  const [movies, setMovies] = useState([]);
+  useEffect(() => {
+    async function FetchAPI() {
+      const response = await fetch(
+        "https://api.themoviedb.org/3/movie/now_playing?api_key=09e9c58d268e354b1e572da3114678bf"
+      );
+      const data = await response.json();
+      setMovies(data.results);
+    }
+    FetchAPI();
+  }, []);
   return (
-    <div>
-      MovieList
+    <div className="max-w-7xl mx-auto py-7">
+      <div className="flex justify-start flex-wrap">
+        {movies.map((movie) => (
+          <Card key={movie.id} movies={movie} />
+        ))}
+      </div>
     </div>
-  )
+  );
 }
 
-export default MovieList
+export default MovieList;
