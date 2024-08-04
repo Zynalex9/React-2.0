2ff3818e645ab6b86f22559eacb74100166f08ae
Zynalex9 <alzn952@gmail.com>
2024-08-04 19:29:10 +0500

    Basic Redux Setup

---
+++ Redux Shopping Cart/src/store/cartSlice.js
@@ -0,0 +1,18 @@
+import { createSlice } from "@reduxjs/toolkit";
+const cartSlice = createSlice({
+  name: "cart",
+  initialState: {
+    cartList: [],
+    total: 0,
+  },
+  reducers: {
+    add(state, action) {
+      const updatedCartList = state.cartList.concat(action.payload);
+      const total = state.total + action.payload.price;
+      return { ...state, total: total, cartList: updatedCartList };
+    },
+    remove(state, action) {},
+  },
+});
+export const { add, remove } = cartSlice.actions;
+export const cartReducer = cartSlice.reducer;
