0842e6434d74f52df218d4f91c4efb7d3efc6881
Zynalex9 <alzn952@gmail.com>
2024-11-29 18:13:17 +0500

    add total budget component

--- React Redux Finance Manager/src/components/totalBudget.tsx
+++ React Redux Finance Manager/src/components/totalBudget.tsx
@@ -1,5 +1,27 @@
+import { useSelector } from "react-redux";
+import { GrMoney } from "react-icons/gr";
+
 const totalBudget = () => {
-  return <div></div>;
+  const budgets = useSelector((state: any) => state.finance.budget);
+  const totalBudget = budgets.reduce(
+    (sum: number, budget: any) => sum + budget.amount,
+    0
+  );
+  console.log("total budget", totalBudget);
+  return (
+    <div className="shadow-md rounded-lg bg-white w-1/3  p-6">
+      <h3 className="text-2xl font-bold text-center mb-4 text-gray-800">
+        Total Budget
+      </h3>
+      <div className="flex bg-gray-50 items-center justify-between p-4 rounded-lg">
+        <div className="flex items-center space-x-3">
+          <GrMoney size={32} />
+          <span className="text-lg font-medium text-gray-700">Budget:</span>
+        </div>
+        <p className="text-xl font-semibold text-gray-700">{totalBudget ? `$${totalBudget}` : "No Added Budget yet"}</p>
+      </div>
+    </div>
+  );
 };
 
 export default totalBudget;
--- React Redux Finance Manager/src/components/totalIncome.tsx
+++ React Redux Finance Manager/src/components/totalIncome.tsx
@@ -9,7 +9,7 @@ const TotalIncome = () => {
   );
 
   return (
-    <div className="shadow-md rounded-lg p-6 bg-white max-w-sm mx-auto">
+    <div className="shadow-md rounded-lg p-6 bg-white w-1/3">
       <h3 className="text-2xl font-bold text-center mb-4 text-gray-800">
         Total Income
       </h3>
